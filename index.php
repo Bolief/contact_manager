9bc5f93af852356f3d8bcee8c5e7b46d59fac8fc
Francis Bolie <boliefrancis7@gmail.com>
2026-07-22 16:09:41 -0500

    index.php code and folders completed

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 
+<?php
+session_start();
+
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/utilities/functions.php';
+require_once __DIR__ . '/utilities/validation.php';
+require_once __DIR__ . '/utilities/authentication.php';
+require_once __DIR__ . '/models/user_db.php';
+require_once __DIR__ . '/models/contact_db.php';
+
+$controller = filter_input(INPUT_GET, 'controller') ?? 'account';
+
+if ($controller === 'contacts') {
+    require __DIR__ . '/controllers/contact_controller.php';
+} else {
+    require __DIR__ . '/controllers/account_controller.php';
+}
